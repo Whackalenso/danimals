e09bdad423d4e35b9e64545e41d4758093b6c5da
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2022-08-06 18:57:58 -0700

    new icons

--- src/App.js
+++ src/App.js
@@ -36,15 +36,19 @@ const animals = [
 
 function App() {
   var soundCards = [];
-  var Speaker = <audio id="speaker"></audio>
+  var Speaker = <audio id="speaker"></audio>;
   for (var i = 0; i < animals.length; i++) {
-    soundCards.push(<SoundCard animal={animals[i]} key={i} speaker={Speaker} />);
-}
-return (
+    soundCards.push(
+      <SoundCard animal={animals[i]} key={i} speaker={Speaker} />
+    );
+  }
+  return (
     <div className="App">
       <section className="Info">
         <h1 className="title">Danimals</h1>
-        <h3 className="description">Hover / Click on an animal to here what it sounds like</h3>
+        <h3 className="description">
+          Hover / Click on an animal to here what it sounds like
+        </h3>
       </section>
       <div className="sound-cards">{soundCards}</div>
     </div>
